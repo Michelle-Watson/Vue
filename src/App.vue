3e94a9ce96511851435e472b639dfe39d8e1e643
Michelle Watson <alicerockshard@gmail.com>
2019-11-10 16:34:41 -0500

    Began adding components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
+<!--
+Loads the index.html page, everything comes through main.js, and through main App.vue (this)
+-->
+
+<!-- Markup, output -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--
+      Embed DisplayTodos.vue
+    Display todo objects (todoItems), pass it in with a directive (V bind)
+    Bind the tasksToDo (we just called it that, it could be called something else)
+    Pass in todoItems from the data as a prop
+    1st word can be anything, 2nd must match what we called it in data ()
+    -->
+    <Todos v-bind:tasksToDoProp="todoObj"/>
+    
   </div>
 </template>
 
+<!-- Store data from 'todo' list in components as objects -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos
+  },
+  data (){
+    return {
+      // array of objects
+      todoObj: [
+        {
+            id: 1,
+            title: "Item 1",
+            completed: false
+        },
+        {
+            id: 2,
+            title: "Item 2",
+            completed: true
+        }
+      ]
+    }
   }
 }
 </script>
 
+<!-- Global to app -->
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- Add div bc we need a root element -->
+    <div>
+        <!--
+        _____ in: Can be named anything
+        in ____: pertains to the props that are passed in
+        Array: need to loop through the todoItems
+        In 2.2.0+, when using v-for with a component, a key is now required.
+        -->
+       <div v-for="loopThruTasks in tasksToDoProp" :key="loopThruTasks.id"> >
+           <!-- Output TodoItem, need to bind to a prop-->
+           <TodoItem v-bind:loopThruTasks="loopThruTasks"/>
+       </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["tasksToDoProp"] // accepting these as props, now have access to them in the template
+}
+</script>
+
+<style scoped>
+
+</style>
