56938522ed437d9929c5bb9c4330a899b1aea6de
Michelle Watson <alicerockshard@gmail.com>
2019-11-12 17:46:12 -0500

    Added helper comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+// entry point to vue
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // render app. component inside of a div / element w/ the ID of app
+  // Recall: index.html: <div id="app"></div>
   render: h => h(App),
 }).$mount('#app')
